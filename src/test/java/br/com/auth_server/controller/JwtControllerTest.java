a38c206e9ee6d5d43a048b89c205f408e1323b5d
Aline <alineakaki1@gmail.com>
2024-10-22 12:24:09 -0300

    tests: unit testes for JwtController.

--- src/test/java/br/com/auth_server/controller/JwtControllerTest.java
+++ src/test/java/br/com/auth_server/controller/JwtControllerTest.java
@@ -34,5 +34,6 @@ class JwtControllerTest {
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(token, Objects.requireNonNull(response.getBody()).getToken());
+        assertEquals("Bearer", response.getBody().getTokenType());
     }
 }
