a9a3eb26d196890fda2cc900c5cb4325100fba87
Aline <alineakaki1@gmail.com>
2024-10-23 20:41:24 -0300

    remove: removed application-test.properties.

--- src/test/java/br/com/auth_server/service/impl/AssetsServiceImplTest.java
+++ src/test/java/br/com/auth_server/service/impl/AssetsServiceImplTest.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class AssetsServiceImplTest {
 
